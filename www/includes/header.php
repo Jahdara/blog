bfbf192a9e03943e2fe17406e94ddb121f5b6ba3
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-03 11:53:41 +0100

    editing styles

--- www/includes/header.php
+++ www/includes/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
-	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link rel="stylesheet" type="text/css" href="style/styles.css">
 </head>
 <body>
 	<section>
