159b356580bdf2d27334906e296a8a75055f7d66
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-03 11:55:01 +0100

    correcting styles

--- www/includes/dashboard_header.php
+++ www/includes/dashboard_header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title>TEST</title>
-	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link rel="stylesheet" type="text/css" href="style/styles.css">
 </head>
 
 <body>
@@ -10,10 +10,10 @@
 			<h1>T<span>SSB</span></h1>
 			<nav>
 				<ul class="clearfix">
-					<li><a href="add_category.php" <?php Utils::curNav("add_category.php"); ?>>add category</a></li>
-					<li><a href="view_category.php" <?php Utils::curNav("view_category.php"); ?>>view category</a></li>
-					<li><a href="addproduct.php" <?php Utils::curNav("addproduct.php"); ?>>add product</a></li>
-					<li><a href="viewproduct.php" <?php Utils::curNav("viewproduct.php"); ?>>view product</a></li>
+					<li><a href="add_post.php" <?php Utils::curNav("add_post.php"); ?>>add post</a></li>
+					<li><a href="view_post.php" <?php Utils::curNav("view_post.php"); ?>>view post</a></li>
+					<li><a href="edit_post.php" <?php Utils::curNav("edit_post.php"); ?>>edit post</a></li>
+					<li><a href="delete_post.php" <?php Utils::curNav("delete_post.php"); ?>>delete post</a></li>
 				</ul>
 			</nav>
 		</div>
