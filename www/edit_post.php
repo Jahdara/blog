5f93640846b21899309b26b49863d8f9a327d764
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-03 17:18:18 +0100

    php write

---
+++ www/edit_post.php
@@ -0,0 +1,27 @@
+<?php
+
+		session_start();
+
+		$title = 'Blog:Edit Post';
+
+		include 'includes/db.php';
+
+		include 'includes/functions.php';
+
+		include 'includes/header.php';
+
+
+?>
+
+
+
+
+
+
+
+<?php
+
+
+	include 'includes/footer.php';
+
+?>
