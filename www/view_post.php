2c05827b999a15a914158b3e235f7fd79667c605
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-03 15:39:09 +0100

    view

--- www/view_post.php
+++ www/view_post.php
@@ -2,6 +2,8 @@
 
 	session_start();
 
+	$title = "Blog:Register";
+
 	include 'includes/functions.php';
 
 	include 'includes/db.php';
@@ -12,7 +14,31 @@
 
 ?>
 
+<div class="wrapper">
+	<div id="stream">
+			<table id="tab">
+				<thead>
+					<tr>
+						<th>Post Id</th>
+						<th>Date</th>
+						<th>Post Title</th>
+						<th>content</th>
+						<th>firstname</th>
+					</tr>
+				</thead>
+				<tbody>
+				
+						<?php
 
+							$view =viewPost($con); echo $view; 
+
+						?>			
+
+          		</tbody>
+			</table>
+		</div>
+
+		
 
 
 
