a8745df6e1784ee3d7fb1a4023bd65564b68d6c2
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-03 10:39:02 +0100

    continuation validation n Utils

--- www/add_post.php
+++ www/add_post.php
@@ -1,6 +1,34 @@
 <?php
 
-	
+	session_start();
+
+	include 'includes/db.php';
+
+	Utils::checkLogin();
+
+	include 'includes/functions.php';
+
+	include 'include/dashboard_header.php';
+
+	$errors[];
+
+	if(array_key_exists('add',$_POST)){
+
+		if(empty($_POST ['postt'])){
+			$errors['postt']= "Please Enter your Post Title";
+		}
+
+		if(empty($_POST ['cont'])){
+			$errors['cont']= "Please Enter your Content";
+		}
+
+		if(empty($errors)){
+			$clean = array_map('trim', $_POST);
+			Utils::addPost($con, $clean);
+		}
+	}
+
+
 
 ?>
 
@@ -14,13 +42,17 @@
 
 
 				<h3>Post Title</h3>
-				<input type="text" name="Postt" placeholder="Enter Title"/>
+				<input type="text" name="postt" placeholder="Enter Title"/>
 
 
-				
+				<?php
+
+					Utils::displayErrors($errors,'post');
+			
+				?>
 
 				<h3>Content:</h3>
-				<textarea name="con" placeholder="Enter Content"></textarea>
+				<textarea name="cont" placeholder="Enter Content"></textarea>
 
 				<input type="submit" name="add" value="Add">
 				</form>
