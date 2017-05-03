1d51084c50d2dc21d6e049419f2b5cc22d16d3bb
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-03 05:56:21 +0100

    html

---
+++ www/add_post.php
@@ -0,0 +1,29 @@
+<?php
+
+	
+
+?>
+
+
+
+<div class="wrapper">
+		<div id="stream">
+
+				<form id= "register" action= "add_post.php" method="POST">
+
+
+
+				<h3>Post Title</h3>
+				<input type="text" name="Postt" placeholder="Enter Title"/>
+
+
+				
+
+				<h3>Content:</h3>
+				<textarea name="con" placeholder="Enter Content"></textarea>
+
+				<input type="submit" name="add" value="Add">
+				</form>
+				
+				</div>
+	</div>
