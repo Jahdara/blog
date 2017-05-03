d76fb0e5bbd47db3797072a2a332ea2012398365
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-03 17:24:50 +0100

    form action

--- www/edit_post.php
+++ www/edit_post.php
@@ -13,7 +13,24 @@
 
 ?>
 
+ <div class="wrapper">
+		<h1 id="register-label">Add Post</h1>
+		<hr>
+		<form id="register"  action ="" method ="POST">
+			<div>
+				<label>Title:</label>
+				<input type="text" name="title" placeholder="POST TITLE" value="<?php echo $view['title']; ?>">
+			</div>
 
+			<div>
+				<label>post:</label>
+				<textarea name="post" placeholder="INPUT POST HERE" cols="50" rows="10" value="<?php echo $view['posts']; ?>">					
+				</textarea>
+				
+			</div>
+			
+			<input type="submit" name="submit" value="ADD POST">
+		</form>
 
 
 
