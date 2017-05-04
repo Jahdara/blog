400cde3b800b283e701456bca58215ce70895e8b
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-05-04 11:00:57 +0100

    removing edit cur

--- www/includes/dashboard_header.php
+++ www/includes/dashboard_header.php
@@ -12,8 +12,7 @@
 				<ul class="clearfix">
 					<li><a href="add_post.php" <?php Utils::curNav("add_post.php"); ?>>add post</a></li>
 					<li><a href="view_post.php" <?php Utils::curNav("view_post.php"); ?>>view post</a></li>
-					<li><a href="edit_post.php" <?php Utils::curNav("edit_post.php"); ?>>edit post</a></li>
-					<li><a href="delete_post.php" <?php Utils::curNav("delete_post.php"); ?>>delete post</a></li>
+					
 				</ul>
 			</nav>
 		</div>
